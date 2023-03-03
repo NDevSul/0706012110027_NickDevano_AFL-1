af188c1efe6bae747e8bd2f13c5209cb7ddbedc9
nick <nsulistio@student.ciputra.ac.ic>
2023-03-03 10:35:37 +0700

    .

--- 0706012110027_NickDevano_AFL 1/Forest.swift
+++ 0706012110027_NickDevano_AFL 1/Forest.swift
@@ -7,25 +7,25 @@
 
 import Foundation
 
-var trollhealth : Int = 100
 
-let forest: String = """
- As  you enter the forest, you feel a sense of unease wash over you. Suddenly, you hear the sound of twigs snapping behind you, you quickly spin around, and find a Troll emerging from the shadows.
- 
- 😈 Troll name : Jumbo Troll
- 😈 Health : \(trollhealth)
- 
- Choose your action :
- [1] Physical attack. No mana required. Deal 5pt Damage
- [2] Meteor. use 15pt of MP. Deal 50pt of damage
- [3] Shield. use 10 pt of MP. Block enemy's attack in 1 turn.
- 
- [4] Use potion to heal wound
- [5] Scan enemy's vital
- [6] Flee from battle
- 
- Whats your choice
- """
+
+//let forest: String = """
+// As  you enter the forest, you feel a sense of unease wash over you. Suddenly, you hear the sound of twigs snapping behind you, you quickly spin around, and find a Troll emerging from the shadows.
+// 
+// 😈 Troll name : Jumbo Troll
+// 😈 Health : \(trollhealth)
+// 
+// Choose your action :
+// [1] Physical attack. No mana required. Deal 5pt Damage
+// [2] Meteor. use 15pt of MP. Deal 50pt of damage
+// [3] Shield. use 10 pt of MP. Block enemy's attack in 1 turn.
+// 
+// [4] Use potion to heal wound
+// [5] Scan enemy's vital
+// [6] Flee from battle
+// 
+// Whats your choice
+// """
 
 //var forestchoice = Int(readLine()!)
 //switch forestchoice {
