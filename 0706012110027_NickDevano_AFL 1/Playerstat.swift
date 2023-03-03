8158e82d190ad4c8620c89c348a6896fc6056756
nick <nsulistio@student.ciputra.ac.ic>
2023-03-03 10:22:53 +0700

    AFL WEEK 2

---
+++ 0706012110027_NickDevano_AFL 1/Playerstat.swift
@@ -0,0 +1,24 @@
+//
+//  Playerstat.swift
+//  0706012110027_NickDevano_AFL 1
+//
+//  Created by MacBook Pro on 03/03/23.
+//
+
+import Foundation
+
+
+let playerstat = """
+Player Name : \(Nama)
+HP : \(playerhealth)
+MP : \(playermana)
+Magic :
+- Physical attack. No mana required. Deal 5pt Damage
+- Meteor. use 15pt of MP. Deal 50pt of damage
+- Shield. use 10 pt of MP. Block enemy's attack in 1 turn.
+
+Items :
+- Potion x10. Heal 20pt of your HP.
+- Elixir x5. Add 10pt of your MP
+"""
+
--- 0706012110027_NickDevano_AFL 1/main.swift
+++ 0706012110027_NickDevano_AFL 1/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+var playerhealth = 100
+var playermana = 50
+
+let openingstory : String = "Welcome to a world of magic 🧙🏿‍♂️\nYou have been chosen to embark on an epic journey as a young wizard  on the path to becoming a master of the arcane arts. Your adventure will take you through forests 🌲, Mountains 🗻, and dungeon 🏰, where you will face challenges, make allies, and fight enemies \nPress [Return] to continue or [N] to quit: "
+
+for _ in 1... {
+    print(openingstory)
+    let choice1 = readLine()
+    if choice1 == ""{
+        break
+    }else if choice1?.lowercased() == "N"{
+        print("Okay.. See you next time young wizard")
+    }
+}
+
+var Nama: String = ""
+
+Nama = readLine() ?? "";
+
+if Nama == ""  {
+   print("Im assuming your name is Guest")
+} else {
+    print("Nice to meet you \(Nama)")
+}
+
+let starting = """
+From here, you can...
+[C]heck your health and stats
+[H]eal your wounds with potion
+
+Or.. you can choose where you want to go
+
+[F]orest or trolls
+[M]ountain of golem
+[Q]uit the game
+
+Your choice young wizard.. :
+"""
+
+print(forest)
+
 
